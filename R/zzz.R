087d24064f99160c0655348db26dac27e9c6d188
seokhoonj <seokhoonj@gmail.com>
2025-10-23 10:56:16 +0900

    Modify `.onLoad()`, `.onUnload()`

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,27 @@
 
-.INSTEAD_ENV <- NULL
+.INSTEAD_ENV <- new.env(parent = emptyenv())
+
 .onLoad <- function(libname, pkgname) {
-  .INSTEAD_ENV <<- new.env()
-  assign(".DATE_FORMAT", "^\\d{4}(0?[13578]|1[02])(0?[1-9]|[12][0-9]|3[01])$|^\\d{4}(0?[469]|11)(0?[1-9]|[12][0-9]|30)$|^\\d{4}(02)(0?[1-9]|[12][0-9])$|^\\d{4}-(0?[13578]|1[02])-(0?[1-9]|[12][0-9]|3[01])$|^\\d{4}-(0?[469]|11)-(0?[1-9]|[12][0-9]|30)$|^\\d{4}-(02)-(0?[1-9]|[12][0-9])$|^\\d{4}/(0?[13578]|1[02])/(0?[1-9]|[12][0-9]|3[01])$|^\\d{4}/(0?[469]|11)/(0?[1-9]|[12][0-9]|30)$|^\\d{4}/(02)/(0?[1-9]|[12][0-9])$|^\\d{4}\\.(0?[13578]|1[02])\\.(0?[1-9]|[12][0-9]|3[01])$|^\\d{4}\\.(0?[469]|11)\\.(0?[1-9]|[12][0-9]|30)$|^\\d{4}\\.(02)\\.(0?[1-9]|[12][0-9])$",
-         envir = .INSTEAD_ENV)
+  assign(
+    ".DATE_FORMAT",
+    paste0(
+      "^\\d{4}(0?[13578]|1[02])(0?[1-9]|[12][0-9]|3[01])$|",
+      "^\\d{4}(0?[469]|11)(0?[1-9]|[12][0-9]|30)$|",
+      "^\\d{4}(02)(0?[1-9]|[12][0-9])$|",
+      "^\\d{4}-(0?[13578]|1[02])-(0?[1-9]|[12][0-9]|3[01])$|",
+      "^\\d{4}-(0?[469]|11)-(0?[1-9]|[12][0-9]|30)$|",
+      "^\\d{4}-(02)-(0?[1-9]|[12][0-9])$|",
+      "^\\d{4}/(0?[13578]|1[02])/(0?[1-9]|[12][0-9]|3[01])$|",
+      "^\\d{4}/(0?[469]|11)/(0?[1-9]|[12][0-9]|30)$|",
+      "^\\d{4}/(02)/(0?[1-9]|[12][0-9])$|",
+      "^\\d{4}\\.(0?[13578]|1[02])\\.(0?[1-9]|[12][0-9]|3[01])$|",
+      "^\\d{4}\\.(0?[469]|11)\\.(0?[1-9]|[12][0-9]|30)$|",
+      "^\\d{4}\\.(02)\\.(0?[1-9]|[12][0-9])$"
+    ),
+    envir = .INSTEAD_ENV
+  )
+  lockBinding(".DATE_FORMAT", .INSTEAD_ENV)
+
   op <- options()
   op.instead <- list(
     instead.eps = 1e-8,
@@ -11,11 +29,15 @@
     instead.guess_max = 21474836,
     instead.scipen = 14
   )
-  toset <- !(names(op.instead) %in% names(op))
-  if (any(toset)) options(op.instead[toset])
+  toset <- setdiff(names(op.instead), names(op))
+  if (length(toset)) options(op.instead[toset])
+
   invisible()
 }
 
 .onUnload <- function(libpath) {
-  library.dynam.unload("instead", libpath)
+  # if a DLL named 'instead' is registered, unload it safely
+  if ("instead" %in% names(getLoadedDLLs())) {
+    library.dynam.unload("instead", libpath)
+  }
 }
